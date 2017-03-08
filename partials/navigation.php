f9b3c2ccfb090cd80120d30c940ca68c8ddb88ba
Dejan <dejannrt411@gmail.com>
2017-03-08 13:08:42 +0100

    Creating bootstrap navigation

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 8/03/2017
- * Time: 11:45 AM
- */
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a href="index.php" class="navbar-brand">MyGallery</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="upload.php">
+                    <i class="fa fa-lg fa-cloud-upload navbar-icons" aria-hidden="true"></i> Upload photo
+                </a>
+            </li>
+            <li>
+                <a href="login.php">
+                    <i class="fa fa-lg fa-sign-in navbar-icons" aria-hidden="true"></i> Sign in
+                </a>
+            </li>
+            <a href="register.php">
+                <button class="btn btn-primary navbar-btn">Create account</button>
+            </a>
+        </ul>
+    </div>
+</nav>
