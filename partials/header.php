1f27a3010bfa1d1e5eac348ccd992d83b2454536
Dejan <dejannrt411@gmail.com>
2017-03-08 13:09:44 +0100

    Adding html and head with css links

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 8/03/2017
- * Time: 11:47 AM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Google Fonts Lato -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i,900i" rel="stylesheet">
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/ddc4ec2a27.js"></script>
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
