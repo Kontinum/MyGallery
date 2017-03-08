1b56e7faea846513d7bebcce822278a26f7e29ba
Dejan <dejannrt411@gmail.com>
2017-03-08 13:11:34 +0100

    Creating index page

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 8/03/2017
- * Time: 11:41 AM
- */
+    require_once "partials/header.php";
+    require_once "partials/navigation.php";
+?>
+
+<section class="home">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="hero-div">
+                <h1 class="hero-text">
+                    <span class="hero-logo">MyGallery</span> is your place to upload images
+                </h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <p class="hero-paragraph">
+                You can upload up to 16MB photo with jpg, png and bmp extension
+            </p>
+        </div>
+
+        <div class="row">
+            <form class="upload-form" action="" method="post">
+                <input type="file" name="upload_photo" id="upload-photo" class="upload-photo">
+                <label for="upload-photo">Upload image</label>
+            </form>
+        </div>
+    </div>
+</section>
+
+<?php require_once "partials/footer.php" ?>
