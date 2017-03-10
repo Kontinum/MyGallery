55b022b97415321a485f50b7ef9e0bef84ce7e3a
Dejan <dejannrt411@gmail.com>
2017-03-10 14:25:52 +0100

    Starting a session, defining config, autoload classes and importing function file

--- core/init.php
+++ core/init.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 10/03/2017
- * Time: 2:13 PM
- */
+session_start();
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'db_name' => 'mygallery',
+        'username' => 'root',
+        'password' =>''
+    ],
+    'session' => [
+        'session_name' => 'user'
+    ],
+    'remember' => [
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ],
+    'upload' => [
+        'directory' => $_SERVER['DOCUMENT_ROOT'].'/storage'
+    ]
+];
+
+
+spl_autoload_register(function($className){
+   require_once "classes/".$className.".php";
+});
+
+require_once "functions/sanitize.php";
