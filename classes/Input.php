ae75e4ccfdd027898761feb4e5001070ce197f19
Dejan <dejannrt411@gmail.com>
2017-03-10 18:06:33 +0100

    Adding methods to Input class

--- classes/Input.php
+++ classes/Input.php
@@ -1,12 +1,28 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 10/03/2017
- * Time: 5:52 PM
- */
 class Input
 {
+    public static function exists($type = "post")
+    {
+        switch ($type){
+            case 'post' :
+                return (!empty($_POST)) ? true : false;
+            break;
 
+            case 'get':
+                return (!empty($_GET)) ? true : false;
+            break;
+        }
+        return false;
+    }
+
+    public static function get($name,$filterName = "")
+    {
+        if(isset($_POST[$name])){
+            return filter_input(INPUT_POST,$name,$filterName);
+        }else if (isset($_GET[$name])){
+            return filter_input(INPUT_GET,$name,$filterName);
+        }else{
+            return "";
+        }
+    }
 }
