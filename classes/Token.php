2caa839a0b01004ed65dd3f0429f5389700785da
Dejan <dejannrt411@gmail.com>
2017-03-11 17:53:10 +0100

    Creating methods in Token class

--- classes/Token.php
+++ classes/Token.php
@@ -1,12 +1,18 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 11/03/2017
- * Time: 4:23 PM
- */
 class Token
 {
+    public static function generate()
+    {
+        return Session::put(Config::get('session/token_name'),md5(random_bytes(8)));
+    }
 
+    public static function check($token)
+    {
+        $tokenName = Config::get('session/token_name');
+
+        if(Session::exists($tokenName) && Session::get($tokenName) === $token){
+            return true;
+        }
+        return false;
+    }
 }
