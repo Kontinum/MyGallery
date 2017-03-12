9d52988640dc52a6d1513a19769b052c8d7896f2
Dejan <dejannrt411@gmail.com>
2017-03-12 18:24:47 +0100

    Adding Bootstrap js CDN link

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,8 @@
-
+<!-- jQuery CDN -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Bootstrap js CDN -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Custom js -->
 <script src="assets/js/script.js"></script>
     </body>
 </html>
