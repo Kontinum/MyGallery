c010cafc82d9b4801926fb3d1ef0a03079cdc23f
Dejan <dejannrt411@gmail.com>
2017-03-12 16:44:42 +0100

    Creating login and found methods, userData, isLoggedIn and sessionName properties and defining sessionName through the constructor

--- classes/User.php
+++ classes/User.php
@@ -1,17 +1,48 @@
 <?php
 class User
 {
-    private $db = null;
+    private $db = null,
+            $userData,
+            $isLoggedIn = false,
+            $sessionName;
 
     public function __construct()
     {
         $this->db = Database::getInstance();
+        $this->sessionName = Config::get('session/session_name');
     }
 
     public function register($values = array())
     {
         if($this->db->insert('users',$values)){
             return true;
+        }
+        return false;
+    }
+
+    public function found($user)
+    {
+        $field = (is_numeric($user)) ? 'id' : 'username';
+
+        $userCheck = $this->db->get('users',[$field,'=',$user]);
+
+        if($userCheck->count()){
+            $this->userData = $userCheck->first();
+            return true;
+        }
+        return false;
+    }
+
+    public function login($username,$password)
+    {
+        $user = $this->found($username);
+
+        if($user){
+            if(Hash::make($password,$this->userData->salt) === $this->userData->password){
+                Session::put($this->sessionName,$this->userData->id);
+                $this->isLoggedIn = true;
+                return true;
+            }
         }
         return false;
     }
