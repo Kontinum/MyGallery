7f31a5363b96fb3f76eb8644b33cd1706d128a23
Dejan <dejannrt411@gmail.com>
2017-03-12 19:31:20 +0100

    Adding login check to navigation in order to show different menus

--- partials/navigation.php
+++ partials/navigation.php
@@ -9,14 +9,26 @@
                     <i class="fa fa-lg fa-cloud-upload navbar-icons" aria-hidden="true"></i> Upload photo
                 </a>
             </li>
-            <li>
-                <a href="login.php">
-                    <i class="fa fa-lg fa-sign-in navbar-icons" aria-hidden="true"></i> Sign in
-                </a>
-            </li>
-            <a href="register.php">
-                <button class="btn btn-primary navbar-btn">Create account</button>
-            </a>
+            <?php
+                $user = new User();
+                if($user->isLoggedIn()) : ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <?= $user->userData()->username ?> <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                        </ul>
+                    </li>
+                    <?php else: ?>
+                    <li>
+                        <a href="login.php">
+                            <i class="fa fa-lg fa-sign-in navbar-icons" aria-hidden="true"></i> Sign in
+                        </a>
+                    </li>
+                    <a href="register.php">
+                        <button class="btn btn-primary navbar-btn">Create account</button>
+                    </a>
+                <?php endif ?>
         </ul>
     </div>
 </nav>
