53c947e9137dabcf3a4a8502bff326a52d5fcff4
Dejan <dejannrt411@gmail.com>
2017-03-13 13:53:31 +0100

    Creating methods in a Cookie class

--- classes/Cookie.php
+++ classes/Cookie.php
@@ -1,12 +1,29 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 13/03/2017
- * Time: 1:35 PM
- */
 class Cookie
 {
+    public static function exists($name)
+    {
+        return isset($_COOKIE[$name]) ? true : false;
+    }
 
+    public static function get($name)
+    {
+        if(self::exists($name)){
+            return $_COOKIE[$name];
+        }
+        return '';
+    }
+
+    public static function put($name,$value,$expiry)
+    {
+        if(setcookie($name,$value,time()+ $expiry,'/')){
+            return true;
+        }
+        return false;
+    }
+
+    public static function delete($name)
+    {
+        self::put($name,'', - 1);
+    }
 }
