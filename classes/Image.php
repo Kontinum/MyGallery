180b7789f1710b1e033ab70756268b9eb1e8ffcc
Dejan <dejannrt411@gmail.com>
2017-03-16 16:58:50 +0100

    Creating properties and methods in Image class

--- classes/Image.php
+++ classes/Image.php
@@ -1,12 +1,25 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 16/03/2017
- * Time: 4:37 PM
- */
 class Image
 {
+    private $db;
 
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function insertImage($fields = array())
+    {
+        $insertImage = $this->db->insert('images',$fields);
+        
+        if($insertImage){
+            return true;
+        }
+        return false;
+    }
+
+    public function allImages($userId)
+    {
+        return $this->db->get('images',['user_id','=',$userId]);
+    }
 }
