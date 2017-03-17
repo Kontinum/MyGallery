93c8d1cd957bd01cd22e10f5c485035b52fa7ee5
Dejan <dejannrt411@gmail.com>
2017-03-17 01:40:44 +0100

    Adding properties and methods in Pagination class

--- classes/Pagination.php
+++ classes/Pagination.php
@@ -1,12 +1,49 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Dejan
- * Date: 17/03/2017
- * Time: 1:19 AM
- */
 class Pagination
 {
+    private $page,
+            $items_per_page,
+            $total_records;
 
+    public function __construct($page,$items_per_page,$total_records)
+    {
+        $this->page = $page;
+        $this->items_per_page = $items_per_page;
+        $this->total_records = $total_records;
+    }
+
+    public function currentPage()
+    {
+        return $this->page;
+    }
+
+    public function totalPages()
+    {
+        return ceil($this->total_records/$this->items_per_page);
+    }
+
+    public function next()
+    {
+        return $this->page + 1;
+    }
+
+    public function previous()
+    {
+        return $this->page -1;
+    }
+
+    public function hasNext()
+    {
+        return ($this->next() <= $this->currentPage()) ? true : false;
+    }
+
+    public function hasPrevious()
+    {
+        return ($this->previous() >= 1) ? true : false;
+    }
+
+    public function offset()
+    {
+        return ($this->currentPage()-1) * $this->items_per_page;
+    }
 }
