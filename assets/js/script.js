cb011eee236f9be2ae58f51418fb562ab5988630
Dejan <dejannrt411@gmail.com>
2017-03-21 15:22:49 +0100

    Hiding image-edit form and toggling on icon click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
  $('document').ready(function(){
+     //Info box and session messages
      $('.info-box').delay(3000).slideUp(1000);
+
+     //Form for image name editing
+     $('.image-edit-name').hide();
+     $('.image-edit-icon').click(function(){
+         $('.image-edit-name').toggle(700);
+     });
  });
