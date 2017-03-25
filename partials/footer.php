0712a499dd9c9ff293228f20750c26d525d4db62
Dejan <dejannrt411@gmail.com>
2017-03-25 23:26:55 +0100

    Removing jQuery CDN link

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,3 @@
-<!-- jQuery CDN -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <!-- Bootstrap js CDN -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- Custom js -->
