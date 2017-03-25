111a93e6641e95a5d91b2b4e7f5efa17410bbc5d
Dejan <dejannrt411@gmail.com>
2017-03-25 23:52:34 +0100

    Creating imageGallery page

---
+++ imageGallery.php
@@ -0,0 +1,54 @@
+<?php
+    require_once "core/init.php";
+    require_once "partials/user_check.php";
+    require_once 'partials/header.php';
+    require_once 'partials/navigation.php';
+
+    $sql = "SELECT COUNT(*) AS count FROM images WHERE user_id = ?";
+    $countImages = Database::getInstance()->query($sql,[$sessionId]);
+    $totalImages = $countImages->first()->count;
+
+    if($totalImages > 0){
+        $sql  ="SELECT * FROM images WHERE user_id = ? ORDER BY uploaded DESC";
+        $userImages = Database::getInstance()->query($sql,[$sessionId]);
+    }
+
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="wrapper">
+            <?php require_once "partials/info-box.php"; ?>
+            <?php if($totalImages == 0) : ?>
+                <p>You don't have any images. Go to <a href="upload.php">Upload page</a> and upload them</p>
+            <?php else: ?>
+                <p>Gallery view</p>
+
+                <div class="images col-lg-12">
+                    <a style="text-decoration: none" title="Switch to normal view" class="" href="images.php?username=<?= escape($user->userData()->username) ?>">
+                        <i class="fa fa-lg fa-table" aria-hidden="true"></i>
+                    </a>
+                    <div id="gallery" style="display: none">
+                        <?php foreach ($userImages->results() as $image) : ?>
+                            <?php
+                            $imgPath = Config::get('upload/directory')."/".$user->getId()."/".$image->save_name;
+                            ?>
+
+                            <img alt="<?= escape($image->name) ?>" src="<?= "storage/".$user->getId()."/".escape($image->save_name) ?>"
+                                 data-image="<?= "storage/".$user->getId()."/".escape($image->save_name) ?>"
+                                 data-description="<?= escape($image->name) ?>">
+                        <?php endforeach ?>
+                    </div>
+                </div>
+            <?php endif ?>
+        </div>
+    </div>
+</div>
+<script>
+    jQuery(document).ready(function(){
+        jQuery("#gallery").unitegallery();
+    });
+</script>
+<?php
+require_once "partials/footer.php";
+?>
